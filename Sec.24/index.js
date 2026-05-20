e42d955ae9a9454f6c6388db35d8c6901b443a8c
Gray <mitchellbadri@gmail.com>
2026-05-20 16:46:29 -0400

    Basic HTTP Requests complete

--- Sec.24/index.js
+++ Sec.24/index.js
@@ -2,8 +2,18 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+app.get("/", (req, res) => {
+    res.send("<h1>Home Page</h1>");
+})
 
+app.get("/contact", (req, res) => {
+    res.send("<p>123-456-7890</p>");
+})
 
-app.listen(port, ()=>{
+app.get("/about", (req, res) => {
+    res.send("<p>I am a Towson Graduate</p>");
+})
+
+app.listen(port, () => {
     console.log(`Server is listening to port ${port}.`);
 })
