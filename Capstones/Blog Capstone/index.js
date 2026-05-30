5f888cb17ac22ef079542cb1d582be4976236f83
Mitch <mgriff30@towson.edu>
2026-05-30 18:36:15 -0400

    Added comments and removed an extraneous import.

--- Capstones/Blog Capstone/index.js
+++ Capstones/Blog Capstone/index.js
@@ -1,16 +1,20 @@
+//import statements for required node modules
 import express from "express";
 import bodyParser from "body-parser";
 import methodOverride from "method-override"
 
+//Initializes the express app and the port we'll use. 
 const app = express();
 const port = 3000;
 
+//Contains the posts in a simple array. Later expand into proper storage. 
 var posts = [];
 
+//Parses each requests body.
 app.use(bodyParser.urlencoded({ extended: true }));
 // When a route sees ?_method in the request it will modify the request's method to the property's value. 
 app.use(methodOverride("_method"));
-//Parses each requests body. 
+//Configures the express app to use the 'public' folder for static resources such as styling. 
 app.use(express.static("public"));
 
 //Landing page route
@@ -23,6 +27,7 @@ app.get("/editor", (req, res) => {
     //Checks if an existing post's id was passed in and modifies the editor's
     //form action accordingly. 
     var action = "";
+    //sets 'postIndex' to the submitted param if available, otherwise it is -1. 
     var postIndex = req.query.postIndex || -1;
     if(postIndex != -1){
         action = `/update?_method=PUT&postIndex=${postIndex}`
@@ -45,7 +50,6 @@ app.get("/posts", (req, res) => {
 
 //New post submission route
 app.post("/create", (req, res) => {
-    //Complete this route to create a new post. 
     posts.push({
         title: req.body.title,
         content: req.body.content,
@@ -55,6 +59,7 @@ app.post("/create", (req, res) => {
 
 //Put update route
 app.put("/update", (req, res) => {
+    //The post's index is passed back to express and that element of 'posts' is replaced. 
     posts[req.query.postIndex] = {
         title: req.body.title,
         content: req.body.content,
@@ -62,12 +67,13 @@ app.put("/update", (req, res) => {
     res.redirect("/posts");
 });
 
+//Post deletion route. 
 app.delete("/delete", (req, res) => {
     posts.splice(req.query.postIndex, 1);
-    console.log(posts);
     res.redirect("/posts");
 });
 
+//Starts the express app listening. 
 app.listen(port, () => {
     console.log(`App listening on port ${port}`);
 });
