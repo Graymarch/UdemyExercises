97e7042ac2e44801c50e519c37b059277e49c161
Gray <mitchellbadri@gmail.com>
2026-06-17 18:00:28 -0400

    First step complete. The app successfully retrieves country codes from Postgres and highlights the corresponding maps on the svg canvas.
    Also comments added.

--- Sec.33/8.3 Travel Tracker/index.js
+++ Sec.33/8.3 Travel Tracker/index.js
@@ -1,13 +1,20 @@
+//Node import statements
 import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
 import dotenv from "dotenv";
 
+//Configuration settings. 
+
+ //Configures the environment file. 
 dotenv.config({path: "./.env"});
 let env = process.env;
 
+//Setup for the Express app. 
 const app = express();
 const port = 3000;
+
+//Setup for the Postgres API. 
 const db = new pg.Client({
   user: env.USER,
   host: env.HOST,
@@ -16,13 +23,36 @@ const db = new pg.Client({
   port: env.PORT
 });
 
-db.connect();
-
+//Middleware for the express app. 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+//Connects to the Postgres database.
+db.connect();
+
+//Attempts to query the database for all visited countries' codes. 
+let visited;
+try {
+  visited = await db.query("SELECT country_code FROM visited_countries");
+} catch (error) {
+  console.error("Query Failed: " + error.message);
+}
+
+//Closes the connection to the database. 
+db.end();
+
+//Index route. 
 app.get("/", async (req, res) => {
-  //Write your code here.
+  //Concatenates the country codes into a comma delineated string. 
+  let codes = "";
+  visited.rows.forEach(country => {
+    codes += country.country_code + ",";
+  });
+
+  res.render("index.ejs", {
+    countries: codes,
+    total: visited.rows.length
+  });
 });
 
 app.listen(port, () => {
