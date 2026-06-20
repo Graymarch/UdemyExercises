13dad1280b0b0586e6a1455e4b987aad0bca6e12
Gray <mitchellbadri@gmail.com>
2026-06-19 20:41:03 -0400

    User can now freely switch between profiles.

--- Sec.33/8.5 Family Travel Tracker/index.js
+++ Sec.33/8.5 Family Travel Tracker/index.js
@@ -1,6 +1,5 @@
 import express from "express";
 import bodyParser from "body-parser";
-import pg from "pg";
 import pool from "./db.js";
 
 const app = express();
@@ -11,43 +10,57 @@ app.use(express.static("public"));
 
 let currentUserId = 1;
 
-let users = [
-  { id: 1, name: "Angela", color: "teal" },
-  { id: 2, name: "Jack", color: "powderblue" },
-];
+let users = [];
 
-async function checkVisisted() {
-  const result = await pool.query("SELECT country_code FROM visited_countries");
+try {
+  let response = await pool.query("SELECT * FROM users");
+  users = response.rows;
+} catch (error) {
+  console.error("Query Failed: " + error);
+}
+
+async function checkVisisted(user_id) {
+  const result = await pool.query(
+    `SELECT country_code 
+    FROM user_visits 
+    INNER JOIN countries c ON country_id = c.id
+    INNER JOIN users u ON user_id = u.id
+    WHERE user_id = $1`, 
+    [user_id]
+  );
   let countries = [];
   result.rows.forEach((country) => {
     countries.push(country.country_code);
   });
+  console.log(countries);
+  
   return countries;
 }
 app.get("/", async (req, res) => {
-  const countries = await checkVisisted();
+  const countries = await checkVisisted(users[0].id);
   res.render("index.ejs", {
     countries: countries,
     total: countries.length,
     users: users,
-    color: "teal",
+    color: users[0].color,
   });
 });
+
 app.post("/add", async (req, res) => {
   const input = req.body["country"];
 
   try {
     const result = await pool.query(
-      "SELECT country_code FROM countries WHERE LOWER(country_name) LIKE '%' || $1 || '%';",
+      `SELECT id FROM countries WHERE LOWER(country_name) LIKE '%' || $1 || '%';`,
       [input.toLowerCase()]
     );
 
     const data = result.rows[0];
-    const countryCode = data.country_code;
+    const country_id = data.id;
     try {
       await pool.query(
-        "INSERT INTO visited_countries (country_code) VALUES ($1)",
-        [countryCode]
+        "INSERT INTO user_visits VALUES ($1, $2)",
+        [currentUserId, country_id]
       );
       res.redirect("/");
     } catch (err) {
@@ -57,7 +70,20 @@ app.post("/add", async (req, res) => {
     console.log(err);
   }
 });
-app.post("/user", async (req, res) => {});
+
+app.post("/user", async (req, res) => {
+  let user_id = req.body.user;
+  currentUserId = user_id;
+
+  const countries = await checkVisisted(user_id);
+  
+  res.render("index.ejs", {
+    countries: countries,
+    total: countries.length,
+    users: users,
+    color: users.find(user => user.id == user_id).color,
+  });
+});
 
 app.post("/new", async (req, res) => {
   //Hint: The RETURNING keyword can return the data that was inserted.
